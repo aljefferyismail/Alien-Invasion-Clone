a0fcc2706079580922316d73d4cf7485b2234020
aljefferyismail <78284420+aljefferyismail@users.noreply.github.com>
2021-11-29 14:01:49 +0000

    Update ship.py

--- ship.py
+++ ship.py
@@ -9,7 +9,7 @@ class Ship(Sprite):
         self.screen = screen
         self.ai_settings = ai_settings
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('/Users/aljefferyismail/Documents/AWS/Python/ship.bmp')
+        self.image = pygame.image.load('/images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
